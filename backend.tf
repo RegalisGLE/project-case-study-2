a74a5de93aa30a6b2edae29248e7aa014254f570
Javan Regalis <javanregalis@outlook.com>
2025-11-14 16:05:13 +0100

    Fixed committed

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstatebucket-stef"
+    bucket         = "tfstatebucket-JR"
     key            = "project/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "terraform-locks"
